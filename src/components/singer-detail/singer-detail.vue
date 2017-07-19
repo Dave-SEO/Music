34ba06c29fe97c87142bf84ca280a2bc977b986e
dave-seo <1244533983@qq.com>
2017-07-19 23:50:23 +0800

    aa

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -4,7 +4,14 @@
 
 <script>
   import {mapGetters} from 'vuex'
+  import {singerDetail} from '@/api/singer'
+  import {createSong} from 'common/js/song'
   export default {
+    data () {
+      return {
+        singerList: []
+      }
+    },
     computed: {
       // getters 最终映射到computed
       // 通过mapGetters 把mapGetters扩展到计算属性里面
@@ -12,8 +19,28 @@
         'singer' // 相当于在vue实例中挂载了一个singer的属性
       ])
     },
+    methods: {
+      _singerDetail () {
+        singerDetail(this.singer.id).then(res => {
+          if (res.code === 0) {
+            this.singerList = this._normalizeSinger(res.data.list)
+            console.log('///////', this.singerList)
+          }
+        })
+      },
+      _normalizeSinger (list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
     created () {
-      console.log(this.singer)
+      this._singerDetail()
     }
   }
 </script>
