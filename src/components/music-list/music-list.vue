007d811ad3cae9bfc1322966497442ef7d8f7c5b
zhang <1393193549@qq.com>
2017-07-24 10:01:11 +0800

    Merge branch 'songlist'
    # Conflicts:
    #	src/components/music-list/music-list.vue
    #	src/components/song-list/song-list.vue

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -2,13 +2,13 @@
   <div class="listWrap">
     <div class="top" :style="{'background-image':`url(${bgImage})`}" ref="bgimg">
       <div class="filter" ref="filter"></div>
+      <div class="player" ref="player">随机播放全部</div>
     </div>
-    <div class="back"> < </div>
+    <div class="back" @click="back"> < </div>
     <div class="bglayer" ref="bglayer"></div>
     <scroll ref="list" @scroll="scroll" :data="songs" class="list" :probeType="probeType" :listenScroll="listenScroll">
       <song-list :songs="songs"></song-list>
     </scroll>
-
   </div>
 </template>
 
@@ -47,7 +47,14 @@
     methods: {
       scroll (pos) {
         this.scrollY = pos.y
+      },
+      back () {
+        this.$router.back()
       }
+    },
+    components: {
+      songList,
+      scroll
     },
     watch: {
       scrollY (newY) {
@@ -63,7 +70,6 @@
           zIndex = 1
           console.log(1)
         } else {
-          console.log(2)
           blur = Math.min(20 * percent, 20)
         }
         this.$refs.bgimg.style.transform = `scale(${scale})`
@@ -71,18 +77,16 @@
         this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
         if (newY < this.minHeight) {
           zIndex = 1
+          this.$refs.player.style.display = 'none'
           this.$refs.bgimg.style.paddingTop = 0
           this.$refs.bgimg.style.height = 40 + 'px'
         } else {
+          this.$refs.player.style.display = 'block'
           this.$refs.bgimg.style.paddingTop = '70%'
           this.$refs.bgimg.style.height = 0
         }
         this.$refs.bgimg.style.zIndex = zIndex
       }
-    },
-    components: {
-      songList,
-      scroll
     }
   }
 </script>
@@ -114,20 +118,33 @@
     left: 0;
     bottom: 0;
     width: 100%;
-    background: #fff;
+    background: #2f2f2f;
     .top {
       position: relative;
       padding-top: 70%;
       height: 0;
       background-size: cover;
       transform-origin: top;
+      .player {
+        text-align: center;
+        position: absolute;
+        bottom: 0.5rem;
+        color: #ffcd32;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        width: 2.7rem;
+        padding: 0.14rem 0;
+        border: 1px solid #ffcd32;
+        border-radius: 2rem;
+      }
     }
     .back {
       position: absolute;
       top: 0.2rem;
       left: 0.3rem;
       font-size: 0.7rem;
-      color: #ffffff;
+      color: #ffcd32;
       font-weight: bolder;
       z-index: 2;
     }
