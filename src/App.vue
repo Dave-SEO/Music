edfac0731fd8c4b55430467896fcef23d0cdea30
zhang <1393193549@qq.com>
2017-07-26 11:03:58 +0800

    player

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <vheader></vheader>
     <tab></tab>
+    <player></player>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
@@ -9,12 +10,14 @@
 </template>
 
 <script>
+  import player from 'components/player/player'
   import vheader from 'components/m-header/header'
   import tab from 'components/tab/tab'
   export default {
     components: {
       vheader,
-      tab
+      tab,
+      player
     }
   }
 </script>
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,7 +1,7 @@
 /**
  * Created by zn on 2017/7/18.
  */
-export const SET_SINGER = 'SET_SINGER'
+export const SET_SINGER = 'SETSINGER'
 export const SET_PLAYING_STATE = 'SET_PLAYING_STATE'
 export const SET_FULL_SCREEN = 'SET_FULL_SCREEN'
 export const SET_PLAYLIST = 'SET_PLAYLIST'
